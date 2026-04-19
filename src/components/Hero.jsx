568750375124f0f693dfb01af9e4693c11eb9434
prince-0303 <princebinil2003@gmail.com>
2026-04-19 16:06:09 +0530

    changed some files to s3

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import heroBg from '../assets/hero_bg.png';
 
 const Hero = () => {
   return (
@@ -10,7 +9,7 @@ const Hero = () => {
       {/* Background Image Container */}
       <div 
         className="absolute inset-0 w-full h-full bg-cover bg-center bg-no-repeat"
-        style={{ backgroundImage: `url(${heroBg})` }}
+        style={{ backgroundImage: `url('https://thapasya-media.s3.amazonaws.com/test/hero_bg.png')` }}
       />
       
       {/* Dark Red Gradient Overlay */}
